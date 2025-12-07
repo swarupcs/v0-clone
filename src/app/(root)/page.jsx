d62d8310b10b5ce221b9f7b6e8306ad96d0f7911
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-07 18:53:49 +0530

    feat: Home Page - Implement initial Next.js app structure with a project idea submission form and templates.

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,12 +1,36 @@
-import { Button } from '@/components/ui/button';
-import { UserButton } from '@clerk/nextjs';
+import ProjectsForm from '@/modules/home/components/project-form';
 import Image from 'next/image';
+import React from 'react';
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <Button>Test</Button>
-      <UserButton />
+    <div className='flex items-center justify-center w-full px-4 py-8'>
+      <div className='max-w-5xl w-full'>
+        <section className='space-y-8 flex flex-col items-center'>
+          <div className='flex flex-col items-center'>
+            <Image
+              src={'/logo.svg'}
+              width={100}
+              height={100}
+              alt='Logo'
+              className='hidden md:block invert dark:invert-0'
+            />
+          </div>
+          <h1 className='text-2xl md:text-5xl font-bold text-center'>
+            Build Something with 💓
+          </h1>
+
+          <p className='text-lg md:text-xl text-muted-foreground text-center'>
+            Create apps and websites by chatting with AI
+          </p>
+
+          <div className='max-w-3xl w-full'>
+            <ProjectsForm />
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
+            <Toaster />
             {children}
           </ThemeProvider>
         </body>
