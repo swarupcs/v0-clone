5609dc37af82b5a17e78f9fc94d396087870b880
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-13 21:41:37 +0530

    feat: Code View and Demo View -  Implement Project View with a file explorer, code viewer, and web fragment preview.

--- src/modules/projects/components/project-view.jsx
+++ src/modules/projects/components/project-view.jsx
@@ -12,9 +12,13 @@ import Link from 'next/link';
 import { useAuth } from '@clerk/nextjs';
 import ProjectHeader from './project-header';
 import MessageContainer from './message-container';
+import { Code, CrownIcon, EyeIcon } from 'lucide-react';
+import FragmentWeb from './fragment-web';
+import { FileExplorer } from './file-explorer';
 
 const ProjectView = ({ projectId }) => {
   const [activeFragment, setActiveFragment] = useState(null);
+  const [tabState, setTabState] = useState('preview');
 
   return (
     <div className='h-screen'>
@@ -34,7 +38,69 @@ const ProjectView = ({ projectId }) => {
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={65} minSize={50}>
-          {/* todo add tabs code and demo */}
+          <Tabs
+            className={'h-full flex flex-col'}
+            defaultValue='preview'
+            value={tabState}
+            onValueChange={(value) => setTabState(value)}
+          >
+            <div className='w-full flex items-center p-2 border-b gap-x-2'>
+              <TabsList className={'h-8 p-0 border rounded-md'}>
+                <TabsTrigger
+                  value='preview'
+                  className={'rounded-md px-3 flex items-center gap-x-2'}
+                >
+                  <EyeIcon className='size-4' />
+                  <span>Demo</span>
+                </TabsTrigger>
+
+                <TabsTrigger
+                  value='code'
+                  className={'rounded-md px-3 flex items-center gap-x-2'}
+                >
+                  <Code className='size-4' />
+                  <span>Code</span>
+                </TabsTrigger>
+              </TabsList>
+
+              <div className='ml-auto flex items-center gap-x-2'>
+                <Button asChild size={'sm'}>
+                  <Link href={'/pricing'}>
+                    <CrownIcon className='size-4 mr-2' />
+                    Upgrade
+                  </Link>
+                </Button>
+              </div>
+            </div>
+
+            <TabsContent
+              value='preview'
+              className={'flex-1 h-[calc(100%-4rem)] overflow-hidden'}
+            >
+              {activeFragment ? (
+                <>
+                  <FragmentWeb data={activeFragment} />
+                </>
+              ) : (
+                <div className='flex items-center justify-center h-full text-muted-foreground'>
+                  Select a fragment to preview
+                </div>
+              )}
+            </TabsContent>
+
+            <TabsContent
+              value='code'
+              className={'flex-1 h-[calc(100%-4rem)] overflow-hidden'}
+            >
+              {activeFragment?.files ? (
+                <FileExplorer files={activeFragment.files} />
+              ) : (
+                <div className='flex items-center justify-center h-full text-muted-foreground'>
+                  Select a fragment to view code
+                </div>
+              )}
+            </TabsContent>
+          </Tabs>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
